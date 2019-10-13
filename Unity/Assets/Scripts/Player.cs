91822de39d1e61a5da3e18b0c8c2f89e802ce2bd
Peyman Azami <peymanazami3@gmail.com>
2019-10-13 14:03:46 +0100

    added player jump and climb

--- Unity/Assets/Scripts/Player.cs
+++ Unity/Assets/Scripts/Player.cs
@@ -6,9 +6,12 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 14f;
+    [SerializeField] float climbSpeed = 5f;
 
     Rigidbody2D playerRigidBody;
     Animator playerAnimator;
+    Collider2D playerCollider2D;
 
     bool isAlive = true;
 
@@ -17,20 +20,53 @@ public class Player : MonoBehaviour
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        playerCollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        run();
+        Run();
+        Jump();
         FlipSprite();
+        ClimbLadder();
     }
 
-    private void run()
+    private void ClimbLadder()
+    {
+        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            playerAnimator.SetBool("Climbing", false);
+            return;
+        }
+
+        float inputAxis = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 ladderVelocity = new Vector2(playerRigidBody.velocity.x, inputAxis * climbSpeed);
+        playerRigidBody.velocity = ladderVelocity;
+
+        bool hasVerticalSpeed = Mathf.Abs(playerRigidBody.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool("Climbing", hasVerticalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return; // don't allow player to jump
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+            playerRigidBody.velocity += jumpVelocity;
+        }
+    }
+
+    private void Run()
     {
         float inputAxis = CrossPlatformInputManager.GetAxis("Horizontal");
-        Vector2 velocity = new Vector2(inputAxis * moveSpeed, playerRigidBody.velocity.y);
-        playerRigidBody.velocity = velocity;
+        Vector2 moveVelocity = new Vector2(inputAxis * moveSpeed, playerRigidBody.velocity.y);
+        playerRigidBody.velocity = moveVelocity;
 
         bool hasHorizontalMovement = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
         playerAnimator.SetBool("Running", hasHorizontalMovement);
