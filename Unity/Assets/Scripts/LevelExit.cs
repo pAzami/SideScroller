a3877d1ae615136dd627c4fc76067ff6f48c25c1
Peyman Azami <peymanazami3@gmail.com>
2019-10-14 14:48:34 +0100

    New levels. Start and Game Compelete menus.

---
+++ Unity/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 2f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<SpriteRenderer>().color = Color.green;
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(loadDelay);
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
