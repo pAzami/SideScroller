41e48145f5acaad96550cf3562c266d3b27d2c32
Peyman Azami <peymanazami3@gmail.com>
2019-10-16 14:05:42 +0100

    timer added to level 3 through dynamic traps

---
+++ Unity/Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    [Tooltip ("Game units per second")]
+    [SerializeField] float rate = 0.4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(0f, rate * Time.deltaTime));
+    }
+}
