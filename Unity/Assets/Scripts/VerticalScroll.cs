00fd961876a206b42d6d06bd843382eef8c2d181
Peyman Azami <peymanazami3@gmail.com>
2019-10-19 14:31:30 +0100

    implemented downward scroll for dynamic traps

--- Unity/Assets/Scripts/VerticalScroll.cs
+++ Unity/Assets/Scripts/VerticalScroll.cs
@@ -7,9 +7,24 @@ public class VerticalScroll : MonoBehaviour
     [Tooltip ("Game units per second")]
     [SerializeField] float rate = 0.4f;
 
+    [Tooltip("Direction of verticle movement (up or down)")]
+    [SerializeField] bool movingUp = true;
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector2(0f, rate * Time.deltaTime));
+        Move();
+    }
+
+    private void Move()
+    {
+        if (movingUp)
+        {
+            transform.Translate(new Vector2(0f, rate * Time.deltaTime));
+        }
+        else
+        {
+            transform.Translate(new Vector2(0f, -rate * Time.deltaTime));
+        }
     }
 }
